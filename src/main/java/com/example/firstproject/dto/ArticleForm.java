452e2f034145429cda15a05de692c30810d2718d
엄혜영 <eomhyeyeong@eomhyeyeongs-MacBook-Air.local>
2024-01-22 09:11:32 +0900

    CH4 롬복과 리펙터링

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // 클래스 안쪽의 모든 필드를 매개변수로 하는 생성자 자동으로 생성
+@ToString           // toString() 메서드 간소화
 public class ArticleForm {
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 데이터를 잘 받았는지 확인하는 메서드
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);

1a79bd14479b255726b14e05416da01311bffad5
EomHyeYeong <hy30051@naver.com>
2024-01-22 12:06:10 +0900

    CH7 게시글 수정하기: Update

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -7,10 +7,11 @@ import lombok.ToString;
 @AllArgsConstructor // 클래스 안쪽의 모든 필드를 매개변수로 하는 생성자 자동으로 생성
 @ToString           // toString() 메서드 간소화
 public class ArticleForm {
-    private String title;
-    private String content;
+    private Long    id;
+    private String  title;
+    private String  content;
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
